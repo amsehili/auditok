9668ada9137438d6667eb5e0237588351935fc00
Amine Sehili <amine.sehili@gmail.com>
2015-11-24 02:40:43 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
