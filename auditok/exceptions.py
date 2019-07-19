0e15a86a7ce2f23363cea534464c93e8d770e523
Amine Sehili <amine.sehili@gmail.com>
2019-07-19 23:16:00 +0100

    Add arguments to TooSamllBlockDuration exception

--- auditok/exceptions.py
+++ auditok/exceptions.py
@@ -4,3 +4,8 @@ class DuplicateArgument(Exception):
 
 class TooSamllBlockDuration(ValueError):
     """Raised when block_dur results in a block_size smaller than one sample"""
+
+    def __init__(self, message, block_dur, sampling_rate):
+        self.block_dur = block_dur
+        self.sampling_rate = sampling_rate
+        super(TooSamllBlockDuration, self).__init__(message)
--- auditok/util.py
+++ auditok/util.py
@@ -709,7 +709,9 @@ class _FixedSizeAudioReader(_AudioSourceProxy):
             err_msg = "Too small block_dur ({0:f}) for sampling rate ({1}). "
             err_msg += "block_dur should cover at least one sample "
             err_msg += "(i.e. 1/{1})"
-            raise TooSamllBlockDuration(err_msg.format(block_dur, self.sr))
+            raise TooSamllBlockDuration(
+                err_msg.format(block_dur, self.sr), block_dur, self.sr
+            )
 
     def read(self):
         return self._audio_source.read(self._block_size)
