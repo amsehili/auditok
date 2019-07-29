29d13c2ead51b763b5ccbf8f9dcca8b1bfec609b
Amine Sehili <amine.sehili@gmail.com>
2019-07-29 20:37:31 +0100

    Move signal processing functions to a separate module

---
+++ auditok/signal.py
@@ -0,0 +1,43 @@
+from array import array
+import math
+
+FORMAT = {1: "b", 2: "h", 4: "i"}
+_EPSILON = 1e-20
+
+
+def to_array(data, fmt):
+    return array(fmt, data)
+
+
+def extract_single_channel(data, fmt, channels, selected):
+    samples = array(fmt, data)
+    return samples[selected::channels]
+
+
+def average_channels(data, fmt, channels):
+    all_channels = array(fmt, data)
+    mono_channels = [
+        array(fmt, all_channels[ch::channels]) for ch in range(channels)
+    ]
+    avg_arr = array(
+        fmt, (sum(samples) // channels for samples in zip(*mono_channels))
+    )
+    return avg_arr
+
+
+def separate_channels(data, fmt, channels):
+    all_channels = array(fmt, data)
+    mono_channels = [
+        array(fmt, all_channels[ch::channels]) for ch in range(channels)
+    ]
+    return mono_channels
+
+
+def calculate_energy_single_channel(x):
+    energy = max(sum(i ** 2 for i in x) / len(x), 1e-20)
+    return 10 * math.log10(energy)
+
+
+def calculate_energy_multichannel(x, aggregation_fn=max):
+    energies = (calculate_energy_single_channel(xi) for xi in x)
+    return aggregation_fn(energies)
---
+++ auditok/signal_numpy.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+FORMAT = {1: np.int8, 2: np.int16, 4: np.int32}
+_EPSILON = 1e-20
+
+
+def to_array(data, fmt):
+    return np.frombuffer(data, dtype=fmt).astype(np.float64)
+
+
+def extract_single_channel(data, fmt, channels, selected):
+    samples = np.frombuffer(data, dtype=fmt)
+    return samples[selected::channels].astype(np.float64)
+
+
+def average_channels(data, fmt, channels):
+    array = np.frombuffer(data, dtype=fmt).astype(np.float64)
+    return array.reshape(-1, channels).mean(axis=1)
+
+
+def separate_channels(data, fmt, channels):
+    array = np.frombuffer(data, dtype=fmt).astype(np.float64)
+    return array.reshape(-1, channels).T
+
+
+def calculate_energy_single_channel(x):
+    return 10 * np.log10(np.dot(x, x).clip(min=_EPSILON) / x.size)
+
+
+def calculate_energy_multichannel(x, aggregation_fn=np.max):
+    energy = 10 * np.log10((x * x).mean(axis=1).clip(min=_EPSILON))
+    return aggregation_fn(energy)
