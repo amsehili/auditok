0936719f5f6c7d9e8275e5c9158c8ed488eb86ba
Amine Sehili <amine.sehili@gmail.com>
2019-10-27 15:48:13 +0100

    Update python version in setup and travis config.

--- setup.py
+++ setup.py
@@ -6,26 +6,15 @@ from setuptools import setup
 
 _version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-if sys.version_info >= (3, 0):
-    with open("auditok/__init__.py", "rt") as f:
-        version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
-        long_desc = open("doc/index.rst", "rt").read()
-
-else:
-    with open("auditok/__init__.py", "rb") as f:
-        version = str(
-            ast.literal_eval(
-                _version_re.search(f.read().decode("utf-8")).group(1)
-            )
-        )
-        long_desc = open("doc/index.rst", "rt").read().decode("utf-8")
-
+with open("auditok/__init__.py", "rt") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
+    long_desc = open("README.rst", "rt").read()
 
 setup(
     name="auditok",
     version=version,
     url="http://github.com/amsehili/auditok/",
-    license="GNU General Public License v3 (GPLv3)",
+    license="MIT",
     author="Amine Sehili",
     author_email="amine.sehili@gmail.com",
     description="A module for Audio/Acoustic Activity Detection",
@@ -36,7 +25,6 @@ setup(
     zip_safe=False,
     platforms="ANY",
     provides=["auditok"],
-    requires=["PyAudio"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
@@ -47,11 +35,14 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
