9a9825cb0805174f476a85fadb32c1a685a2a3f8
Amine Sehili <amine.sehili@gmail.com>
2021-01-03 15:28:13 +0100

    Update version

--- auditok/__init__.py
+++ auditok/__init__.py
@@ -2,7 +2,7 @@
 :author:
 
 Amine SEHILI <amine.sehili@gmail.com>
-2015-2019
+2015-2021
 
 :License:
 
@@ -14,4 +14,4 @@ from .io import *
 from .util import *
 from .exceptions import *
 
-__version__ = "0.2.0.alpha"
+__version__ = "0.2.0"
